78774fbdc2ca02cc113b85482fe8f141fea56530
fred-corp <freddruppel@gmail.com>
2022-04-07 17:15:30 +0200

    added commands & added channel hopping

--- dPyMR.py
+++ dPyMR.py
@@ -2,7 +2,7 @@ import time
 import serial
 
 class Transceiver :
-  def __init__(self, dPyMRserial, ownID, timeout = 2):
+  def __init__(self, dPyMRserial, ownID, MSGCH = -1, DEFCH = -1, timeout = 2):
     self.dPyMRserial = dPyMRserial
     self.ownID = ownID
     self.timeout = timeout
@@ -10,6 +10,18 @@ class Transceiver :
 
     self.bol = b'\x02'
     self.eol = b'\x03'
+
+    if MSGCH == -1 :
+      self.MSGCH = self.getChannel()
+    else :
+      self.MSGCH = MSGCH
+    
+    if DEFCH == -1 :
+      self.DEFCH = self.getChannel()
+    else :
+      self.DEFCH = DEFCH
+
+    self.setChannel(self.DEFCH)
 
   def discover(self, idRange = (0, 9999999)) :
     # TODO: Implement this function
@@ -20,8 +32,9 @@ class Transceiver :
     self.dPyMRserial.write(data)
 
   def sendMessage(self, message, otherID):
+    self.setChannel(self.MSGCH, resetDefault = True)
+
     command = '*SET,DPMR,TXMSG,IND,{},{},MSG,"{}",ACK'.format(str(otherID).zfill(7), str(self.ownID).zfill(7), message)
-    
     self.sendCommand(command)
     
     response = b'' 
@@ -30,6 +43,7 @@ class Transceiver :
     while not '*NTF,DPMR,TXMSG,IND,' in str(response.decode('utf-8')) :
       if(time.time() - beginTime > self.timeout):
         if(response == b''):
+          self.setChannel(self.DEFCH)
           return 'TIMEOUT_ERROR'
       byteread = self.dPyMRserial.read() #Read one byte, stuff it in a temp variable
       response += byteread #Add it to the read string
@@ -38,8 +52,50 @@ class Transceiver :
       response += byteread #Add it to the read string
 
     if '"' + message + '",ACK,OK' in str(response.decode('utf-8')) :
+      self.setChannel(self.DEFCH)
       return 'ACK_OK'
     elif '"' + message + '",ACK,NG' in str(response.decode('utf-8')) :
+      self.setChannel(self.DEFCH)
       return 'ACK_NG'
     else :
+      self.setChannel(self.DEFCH)
       return 'UNKNOWN_ERROR'
+
+  def receiveCommand(self):
+    # TODO : Implement this function
+    return None
+
+  def receiveMessage(self):
+    # TODO : Implement this function
+    return None
+
+  def setChannel(self, channel, resetDefault = False):
+    if resetDefault:
+      self.DEFCH = self.getChannel()
+    
+    command = '*SET,MCH,SEL,{}'.format(str(channel))
+    self.sendCommand(command)
+
+  def getChannel(self, resetDefault = False):
+    command = '*GET,MCH,SEL'
+    self.sendCommand(command)
+    response = b''
+    byteread = b''
+    beginTime = time.time()
+    while not '*NTF,MCH,SEL,' in str(response.decode('utf-8')) :
+      if(time.time() - beginTime > self.timeout):
+        if(response == b''):
+          return 'TIMEOUT_ERROR'
+      byteread = self.dPyMRserial.read()
+      response += byteread
+    while not (byteread==self.eol):
+      byteread = self.dPyMRserial.read()
+      response += byteread
+    currentChannel = int(response.decode('utf-8').split(',')[3][:-1])
+    if resetDefault :
+      self.DEFCH = currentChannel
+    return currentChannel
+
+  def setUItext(self, message = ""):
+    command = '*SET,UI,TEXT,"{}"'.format(message)
+    self.sendCommand(command)

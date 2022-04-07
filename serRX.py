784d4a6ee866c6ed1ed55b10608b70309a4d209a
fred-corp <freddruppel@gmail.com>
2022-04-07 15:15:33 +0200

    added serRX.py script

---
+++ serRX.py
@@ -0,0 +1,22 @@
+import pynmea2
+import re
+import serial
+import io
+import requests
+
+
+def sendtraccar(radioid, lat, lon):
+    requests.post('http://mdasilva.eu:5055', data = {'id' : radioid, 'lat': lat, 'lon': lon})
+
+
+ser=serial.Serial('/dev/tty.usbserial-145230', 4800, timeout = 2)
+eol = b'\x03'
+
+while True:
+    response = b'' 
+    byteread = b'' 
+    while not (byteread==eol): #While we haven't received eol
+        byteread = ser.read() #Read one byte, stuff it in a temp variable
+        response += byteread #Add it to the read string
+    datain = str(response.decode('utf-8')) #Convert it  and copy everything into a string
+    print(response)

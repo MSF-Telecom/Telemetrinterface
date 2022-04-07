50b37af0f63f7fe5c37fa1f9e36e52aed3332442
fred-corp <freddruppel@gmail.com>
2022-04-07 16:38:28 +0200

    changed behavior of sendMessage to use sendCommand

--- dPyMR.py
+++ dPyMR.py
@@ -8,10 +8,11 @@ class Transceiver :
     self.timeout = timeout
     self.ownID = ownID
 
+    self.bol = b'\x02'
+    self.eol = b'\x03'
+
   def sendCommand(self, command):
-    bol = b'\x02'
-    eol = b'\x03'
-    data = bol + command.encode("utf-8") + eol
+    data = self.bol + command.encode("utf-8") + self.eol
     self.dPyMRserial.write(data)
 
   def discover(self, idRange = (0, 9999999)) :
@@ -21,10 +22,8 @@ class Transceiver :
 
   def sendMessage(self, message, otherID):
     command = '*SET,DPMR,TXMSG,IND,{},{},MSG,"{}",ACK'.format(str(otherID).zfill(7), str(self.ownID).zfill(7), message)
-    bol = b'\x02'
-    eol = b'\x03'
-    data = bol + command.encode("utf-8") + eol
-    self.dPyMRserial.write(data)
+    
+    self.sendCommand(command)
     
     response = b'' 
     byteread = b''
@@ -35,7 +34,7 @@ class Transceiver :
           return 'TIMEOUT_ERROR'
       byteread = self.dPyMRserial.read() #Read one byte, stuff it in a temp variable
       response += byteread #Add it to the read string
-    while not (byteread==eol): #While we haven't received eol
+    while not (byteread==self.eol): #While we haven't received eol
       byteread = self.dPyMRserial.read() #Read one byte, stuff it in a temp variable
       response += byteread #Add it to the read string
 

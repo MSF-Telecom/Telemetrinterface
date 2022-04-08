209d8bb48a4d4c833655853c326275d53c70de4e
fred-corp <freddruppel@gmail.com>
2022-04-08 11:37:44 +0200

    added help to methods & functions

--- dPyMR.py
+++ dPyMR.py
@@ -3,6 +3,14 @@ import serial
 
 class Transceiver :
   def __init__(self, dPyMRserial, ownID, MSGCH = -1, DEFCH = -1, timeout = 2):
+    """
+    Parameters :
+    • dPyMRserial [serial] : The serial object of the transceiver
+    • ownID [int] : The ID of the transceiver
+    • MSGCH [int] : The channel to use for sending messages, defaults to current set channel
+    • DEFCH [int] : The channel to use for receiving messages, defaults to current set channel
+    • timeout [int] : The global timeout in seconds, defaults to 2
+    """
     self.dPyMRserial = dPyMRserial
     self.ownID = ownID
     self.timeout = timeout
@@ -24,14 +32,35 @@ class Transceiver :
     self.setChannel(self.DEFCH)
 
   def discover(self, idRange = (0, 9999999)) :
+    """
+    • TODO : Implement this method
+    """
     # TODO: Implement this function
     return ()
 
   def sendCommand(self, command):
+    """
+    Parameters :
+    • command [string] : The command to send to the radio
+    Returns :
+    • Nothing
+    """
     data = self.bol + command.encode("utf-8") + self.eol
     self.dPyMRserial.write(data)
 
   def sendMessage(self, message, otherID, timeout = 10, verbose = False):
+    """
+    Parameters :
+    • message [string] : The message to send
+    • otherID [int] : The ID of the receiver
+    • timeout [int] : The timeout in seconds
+    • verbose [bool] : If true, print the response of the radio while the message is being sent
+    Returns :
+    • ACK [string] : The response of the radio :
+      ACK_OK if the message was sent, 
+      ACK_NG if the message was not sent, TIMEOUT_ERROR if the timeout was reached,
+      UNKNOWN_ERROR if an unknown error occured
+    """
     self.setChannel(self.MSGCH, resetDefault = True)
 
     command = '*SET,DPMR,TXMSG,IND,{},{},MSG,"{}",ACK'.format(str(otherID).zfill(7), str(self.ownID).zfill(7), message)
@@ -60,6 +89,12 @@ class Transceiver :
       return 'UNKNOWN_ERROR'
 
   def receiveCommand(self, timeout = 2):
+    """
+    Parameters :
+    • timeout [int] : The timeout in seconds
+    Returns :
+    • response [string] : The response of the radio
+    """
     # TODO : Verify & test function
     response = b''
     byteread = b''
@@ -76,6 +111,13 @@ class Transceiver :
     return response.decode('utf-8')[1:-1]
 
   def receiveMessage(self, timeout = 2, verbose = False):
+    """
+    Parameters :
+    • timeout [int] : The timeout in seconds
+    • verbose [bool] : If true, print the response of the radio while the message is being received
+    Returns :
+    • message [tuple] : The received & parsed message with (senderID [int], RSSI [int], Message [string])
+    """
     # TODO : Verify & test function
     response = ''
     
@@ -90,6 +132,13 @@ class Transceiver :
     return (int(response.split(',')[-5]), int(response.split(',')[-3]), response.split(',')[-1][1:-1])
 
   def setChannel(self, channel, resetDefault = False):
+    """
+    Parameters :
+    • channel [int] : The channel to set
+    • resetDefault [bool] : If true, reset the default channel to the current set channel
+    Returns :
+    • Nothing
+    """
     if resetDefault:
       self.DEFCH = self.getChannel()
     
@@ -97,6 +146,12 @@ class Transceiver :
     self.sendCommand(command)
 
   def getChannel(self, resetDefault = False):
+    """
+    Parameters :
+    • resetDefault [bool] : If true, reset the default channel to the current set channel
+    Returns :
+    • channel [int] : The current channel
+    """
     command = '*GET,MCH,SEL'
     self.sendCommand(command)
     response = b''
@@ -120,5 +175,11 @@ class Transceiver :
     return currentChannel
 
   def setUItext(self, message = ""):
+    """
+    Parameters :
+    • message [string] : The message to display on the UI, if set to "" the UI will be cleared
+    Returns :
+    • Nothing
+    """
     command = '*SET,UI,TEXT,"{}"'.format(message)
     self.sendCommand(command)

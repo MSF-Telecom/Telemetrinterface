247b9971bcfc1832866ca31ea4e7adef441ef1f2
fred-corp <freddruppel@gmail.com>
2022-04-11 15:49:32 +0200

    added corcuitPython dPyMR test

---
+++ python/embedded/dPyMR_test/main.py
@@ -0,0 +1,22 @@
+import lib.dPyMR as dPyMR
+import board
+import busio
+import time
+
+uart = busio.UART(board.GP16, board.GP17, baudrate=9600, timeout=2)
+
+ownID = 1748
+otherID = 1107
+msg = 'This is an embedded test ! :D'
+
+radio = dPyMR.Transceiver(uart, ownID)
+
+while True:
+  msgSent = False
+  while msgSent == False:
+    if radio.sendMessage(msg, otherID) == 'ACK_OK':
+      print('ACK_OK')
+      msgSent = True
+    time.sleep(2)
+
+  time.sleep(5)

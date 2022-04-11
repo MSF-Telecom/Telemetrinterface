ccd304b8b4d7663f26e5493e4b291145d785458e
fred-corp <freddruppel@gmail.com>
2022-04-11 12:11:20 +0200

    added Pico blink test

---
+++ python/embedded/Pico_Blink_Test/main.py
@@ -0,0 +1,12 @@
+import time
+import board
+import digitalio
+
+led = digitalio.DigitalInOut(board.GP25)
+led.direction = digitalio.Direction.OUTPUT
+
+while True:
+    led.value = True
+    time.sleep(0.25)
+    led.value = False
+    time.sleep(0.25)

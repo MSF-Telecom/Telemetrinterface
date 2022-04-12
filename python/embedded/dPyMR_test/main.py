be2c25956f0e743999580cd67c1c1eabf4503c47
fred-corp <freddruppel@gmail.com>
2022-04-12 16:05:52 +0200

    added startup led & pin

--- python/embedded/dPyMR_test/main.py
+++ python/embedded/dPyMR_test/main.py
@@ -2,6 +2,7 @@ import lib.dPyMR as dPyMR
 import board
 import busio
 import time
+import digitalio
 
 uart = busio.UART(board.GP16, board.GP17, baudrate=9600, timeout=2)
 
@@ -10,6 +11,16 @@ otherID = 1107
 msg = 'This is an embedded test ! :D'
 
 radio = dPyMR.Transceiver(uart, ownID)
+
+led = digitalio.DigitalInOut(board.LED)
+pin = digitalio.DigitalInOut(board.GP0)
+led.direction = digitalio.Direction.OUTPUT
+pin.direction = digitalio.Direction.OUTPUT
+led.value = True
+pin.value = True
+time.sleep(1)
+led.value = False
+pin.value = False
 
 while True:
   msgSent = False

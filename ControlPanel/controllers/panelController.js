5783b4e8c15afad2dae878096ece5cb22e4c7cc8
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-05-04 11:01:49 +0200

    added example data for Website Control Panel (#3)

--- ControlPanel/controllers/panelController.js
+++ ControlPanel/controllers/panelController.js
@@ -1,18 +1,24 @@
 const userDB = require('../usersDB.json')
 
 teleData = {
-      1107: {temp: 21.5, hum: 42, press: 1023, 
+      1107: {CPUTemp: [29.4, 36.5], CPUVolt: 1.1, Vers : 1.0, reset : 'none',
+            push: true, pushTime: 10,
+            temp: 21.5, hum: 42, press: 1023,
+            accel : [9.81, 0.0, 0.0],
             in1: false, in2: false, in3: false, in4: false,
             out1: false, out2: false, out3: false, out4: false,
             ain1: 1352, ain2: 532, ain3: 84, vsup: 3045,
             led1 : [34,198,0], led2 : [12,0,230], led3 : [44,27,102],
             led4 : [0,44,128], led5 : [243,14,95]},
-      1748: {temp: 18, hum: 29, press: 998, 
-        in1: false, in2: true, in3: false, in4: false,
-        out1: false, out2: true, out3: false, out4: false,
-        ain1: 204, ain2: 3997, ain3: 256, vsup: 5,
-        led1 : [243,14,95], led2 : [34,198,0], led3 : [0,44,128],
-        led4 : [12,0,230], led5 : [44,27,102]}
+      1748: {CPUTemp: [23.4, 21.5], CPUVolt: 1.1, Vers : 1.0, reset : 'none',
+            push: true, pushTime: 10,
+            temp: 18, hum: 29, press: 998, 
+            accel : [1.51, 3.8, 6.32],
+            in1: false, in2: true, in3: false, in4: false,
+            out1: false, out2: true, out3: false, out4: false,
+            ain1: 204, ain2: 3997, ain3: 256, vsup: 5,
+            led1 : [243,14,95], led2 : [34,198,0], led3 : [0,44,128],
+            led4 : [12,0,230], led5 : [44,27,102]}
           }
 
 exports.controlPage = function (req, res) {

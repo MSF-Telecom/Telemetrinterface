eb35489da68212284708e32a829c878bc32d3a86
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-05-04 12:08:46 +0200

    added some logic to shorten message frames (#16)

--- python/embedded/Telemetrinterface_STD/code.py
+++ python/embedded/Telemetrinterface_STD/code.py
@@ -47,9 +47,20 @@ nodeData = {"CPUTemp": [microcontroller.cpus[0].temperature, microcontroller.cpu
             "led4" : [0,44,128], "led5" : [243,14,95]}
 
 while True:
+  nodeData = {"CPUTemp": [microcontroller.cpus[0].temperature, microcontroller.cpus[0].temperature],
+            "CPUVolt": microcontroller.cpu.voltage, "Vers" : 1.0, "Reset" : microcontroller.cpu.reset_reason,
+            "Push": True, "PushTime": 10,
+            "temp": 21.5, "hum": 42, "press": 1023,
+            "accel" : [9.81, 0.0, 0.0],
+            "in1": False, "in2": False, "in3": False, "in4": False,
+            "out1": False, "out2": False, "out3": False, "out4": False,
+            "ain1": 12.6, "ain2": 4.7, "ain3": 24.2, "vsup": 13.8,
+            "led1" : [34,198,0], "led2" : [12,0,230], "led3" : [44,27,102],
+            "led4" : [0,44,128], "led5" : [243,14,95]}
+
   msgs = ["", "", "", "", "", "", ""]
 
-  SYSmsg = "$SYS,T,{},{},V,{},F,{},R,{}".format(
+  SYSmsg = "$SYS,T,{},{},V,{},F,{},R,'{}'".format(
     nodeData["CPUTemp"][0],
     nodeData["CPUTemp"][1],
     nodeData["CPUVolt"],
@@ -72,7 +83,10 @@ while True:
   msgs[2] = ENVmsg
 
   IOImsg = "$IOI,{},{},{},{}".format(
-    nodeData["in1"], nodeData["in2"], nodeData["in3"], nodeData["in4"]
+    1 if nodeData["in1"] else 0, 
+    1 if nodeData["in2"] else 0,
+    1 if nodeData["in3"] else 0,
+    1 if nodeData["in4"] else 0
   )
   msgs[3] = IOImsg
 
@@ -82,11 +96,14 @@ while True:
   msgs[4] = ANImsg
 
   IOOmsg = "$IOO,{},{},{},{}".format(
-    nodeData["out1"], nodeData["out2"], nodeData["out3"], nodeData["out4"]
+    1 if nodeData["out1"] else 0, 
+    1 if nodeData["out2"] else 0,
+    1 if nodeData["out3"] else 0,
+    1 if nodeData["out4"] else 0
   )
   msgs[5] = IOOmsg
 
-  LEDmsg = "$LED,{},{},{},{},{},{},{},{},{},{},{},{},{},{},{},".format(
+  LEDmsg = "$LED,{},{},{},{},{},{},{},{},{},{},{},{},{},{},{}".format(
     nodeData["led1"][0], nodeData["led1"][1], nodeData["led1"][2],
     nodeData["led2"][0], nodeData["led2"][1], nodeData["led2"][2],
     nodeData["led3"][0], nodeData["led3"][1], nodeData["led3"][2],

596f74f49ae5084b21008e207b9bb608e19b2fe5
fred-corp <7109145+fred-corp@users.noreply.github.com>
2022-05-13 16:37:46 +0200

    changed POST requests behaviors (#21) (#22)

--- ControlPanel/controllers/dPyMRController.js
+++ ControlPanel/controllers/dPyMRController.js
@@ -1,30 +1,22 @@
+const axios = require('axios')
+
 exports.outputs = async function (nodeID, outputs) {
-  await fetch('http://localhost:8081/control/outputs', {
-    method: 'POST',
-    body: JSON.stringify({
+  await axios.post('http://127.0.0.1:8081/control/outputs', {
       nodeID: nodeID,
       outputs: outputs
-    })
   })
 }
 
 exports.buzzer = async function (nodeID, buzzTime) {
-  await fetch('http://127.0.0.1:8081/control/buzzer', {
-    method: 'POST',
-    body: JSON.stringify({
+  await axios.post('http://127.0.0.1:8081/control/buzzer', {
       nodeID: nodeID,
       buzzTime: buzzTime
-    })
   })
 }
 
-exports.leds = async function (nodeID, led, RGB) {
-  await fetch('http://127.0.0.1:8081/control/leds', {
-    method: 'POST',
-    body: JSON.stringify({
+exports.leds = async function (nodeID, RGB) {
+  await axios.post('http://127.0.0.1:8081/control/leds', {
     nodeID: nodeID,
-    leds: led,
     RGB: RGB
-    })
   })
 }
